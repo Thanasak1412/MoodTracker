3a1f40c41daa0693002d713a1dbfa118f8ccf097
Thanasak <thanasak@soilfish.co>
2023-01-29 17:23:08 +0700

    #Updated fixed build.gradle

--- ios/MoodTracker/AppDelegate.mm
+++ ios/MoodTracker/AppDelegate.mm
@@ -46,7 +46,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MoodTracker", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Mood Tracker", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
